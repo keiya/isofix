92a9f4d249d0329dd89fcd8fb4c5c0651517e929
keiya <keiya_21@yahoo.co.jp>
2024-10-06 21:02:52 +0900

    Initial Commit

---
+++ ISOFix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ISOFix
+//
+//  Created by dux on 2024/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ISOFix/ISOFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISOFixApp.swift
+//  ISOFix
+//
+//  Created by dux on 2024/10/06.
+//
+
+import SwiftUI
+
+@main
+struct ISOFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
